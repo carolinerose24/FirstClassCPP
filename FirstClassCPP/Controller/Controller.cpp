ecb1aba39020004bb1ed4a7bd02f5a2ba5475ca4
Caroline Jarman <carolinerosejarman@gmail.com>
2019-01-24 08:30:19 -0700

    Added controller cpp and hpp

---
+++ FirstClassCPP/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  FirstClassCPP
+//
+//  Created by Jarman, Caroline on 1/24/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ FirstClassCPP/Controller/Controller.hpp
@@ -0,0 +1,14 @@
+//
+//  Controller.hpp
+//  FirstClassCPP
+//
+//  Created by Jarman, Caroline on 1/24/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#ifndef Controller_hpp //NO SEMI-COLONS --> read before compiler (linker error)
+#define Controller_hpp
+
+#include <iostream> //the c++ import don't use studi.h
+
+#endif /* Controller_hpp */
